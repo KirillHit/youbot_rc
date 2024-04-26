f4efacdcf84c75c6043b83d8037d79c43998bd13
crocodildo5 <94800874+crocodildo5@users.noreply.github.com>
2024-04-26 22:31:13 +0300

    Общий вид приложения на Qt
    Позже допилю человеческий фикс размеров, где-то внешку подобью

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QLabel>
+#include <QFormLayout>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("ЁБОТ");
+
+    QPalette pal = window.palette();
+    pal.setColor(QPalette::Window, Qt::white);
+    window.setPalette(pal);
+
+    QLabel *label_1 = new QLabel("Linear speed x");
+    QLineEdit *lineEdit_1 = new QLineEdit();
+
+    QLabel *label_2 = new QLabel("Linear speed y");
+    QLineEdit *lineEdit_2 = new QLineEdit();
+
+    QLabel *label_3 = new QLabel("Angle speed z");
+    QLineEdit *lineEdit_3 = new QLineEdit();
+
+    QPushButton *button_1 = new QPushButton("Squeeze claw", &window);
+    QPushButton *button_2 = new QPushButton("Unclench claw", &window);
+
+    QLabel *label_info = new QLabel("Управление рукой");
+
+    QLabel *label_hand_1 = new QLabel("Axis 1");
+    QLineEdit *lineEdit_hand_1 = new QLineEdit();
+    QLabel *label_hand_2 = new QLabel("Axis 2");
+    QLineEdit *lineEdit_hand_2 = new QLineEdit();
+    QLabel *label_hand_3 = new QLabel("Axis 3");
+    QLineEdit *lineEdit_hand_3 = new QLineEdit();
+    QLabel *label_hand_4 = new QLabel("Axis 4");
+    QLineEdit *lineEdit_hand_4 = new QLineEdit();
+    QLabel *label_hand_5 = new QLabel("Axis 5");
+    QLineEdit *lineEdit_hand_5 = new QLineEdit();
+
+
+    QFormLayout *formLayout = new QFormLayout();
+    formLayout -> addRow(label_1, lineEdit_1);
+    formLayout -> addRow(label_2, lineEdit_2);
+    formLayout -> addRow(label_3, lineEdit_3);
+    formLayout -> addRow(button_1, button_2);
+    formLayout -> addRow(label_info);
+    formLayout -> addRow(label_hand_1, lineEdit_hand_1);
+    formLayout -> addRow(label_hand_2, lineEdit_hand_2);
+    formLayout -> addRow(label_hand_3, lineEdit_hand_3);
+    formLayout -> addRow(label_hand_4, lineEdit_hand_4);
+    formLayout -> addRow(label_hand_5, lineEdit_hand_5);
+
+
+    window.setLayout(formLayout);
+
+    window.show();
+
+    return app.exec();
+}
